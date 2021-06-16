0905e14efe8d16947b4703b4aadb21a65e01cec1
omerarslan-devops <60095136+omerarslan-devops@users.noreply.github.com>
2021-06-16 17:44:09 +0300

    Update actions1.groovy

--- vars/actions1.groovy
+++ vars/actions1.groovy
@@ -17,52 +17,7 @@ def runPipeline(Map parameters) {
                   	deploymentType = parameters.deploymentType
                   	projectName = parameters.projectName
             	} 
-
-                stage("Checkout") {
-                  	checkout scm
-            	} 
-
-                stage("Build") {
-                    buildType = getBuildType()
-                    echo "BUILD TYPE:"
-                    echo buildType
-                    if (buildType == "npm") {
-                    	npmBuild()
-                    } else if (buildType == "maven") {
-                    	mavenBuild()
-                    } else if (buildType == "MavenDeploy") {
-                    	mavenDeploy()
-                    } else if (buildType == "LibDeploy") {
-                    	libDeploy()
-                    }  else if (buildType == "S2I") {
-                    	echo "NO BUILD, DIRECT DOCKER BUILD"
-                    } else if (buildType == "quarkus") {
-                    	quarkusBuild()
-                    } 
-            	} 
-
-            	stage ("Dockerize") {
-            		echo "Docker"
-            		if (deploymentType == "KieServer" || buildType == "LibDeploy") {
-            			echo "NO DOCKER BUILD"
-            		} else {
-            			buildAndPushDockerImage()
-            		}
-            		
-            	}
-
-            	stage ("Deploy") {
-            		echo "Deploy"
-					
-            		if (deploymentType == "KieServer" || buildType == "LibDeploy") {
-            			//deployToKieServer()
-						echo "NO DEPLOYMENT"
-            		} else {
-            			triggerDeploymentToOpenshift()
-            		}
-            		
-            	}
-            }
-        }
-} 
+	    }
+    }
+}
 return this
